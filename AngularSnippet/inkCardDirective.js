89602598e61f2726d5bcb8f3452032d5e069fe44
jimmyn413 <jimmytrannguyen@gmail.com>
2016-12-21 08:43:17 -0800

    Add files via upload

---
+++ AngularSnippet/inkCardDirective.js
@@ -0,0 +1,32 @@
+﻿//card directive
+(function () {
+    "use strict";
+
+    angular.module(APPNAME)
+        .directive('contact', myFunction);
+    
+    function myFunction(){
+            return {
+                restrict: 'E',
+                scope: {
+                    cardTemp: '=class',
+                    person: '=ngModel'
+                },
+
+                link: function ($scope) {
+                    $scope.flip = null;
+                    $scope.rotateCard = function () {
+                        $scope.flip = 'hover manual-flip';
+                    };
+                    $scope.unRotateCard = function () {
+                        $scope.flip = null;
+                    }
+                    $scope.$watch('cardTemp', function (cardTemp) {
+                        $scope.newUrl = '/Scripts/sabio/directives/inkCard/templates/' + cardTemp + '.html';
+                    });
+
+                },
+                templateUrl: $scope.newUrl
+            };
+        };
+})();
---
+++ AngularSnippet/momentFilter.js
@@ -0,0 +1,9 @@
+﻿angular.module(APPNAME)
+    .filter('moment', [
+        function () {
+            return function (date, method) {
+                var momented = moment.utc(date);
+                return momented[method].apply(momented, Array.prototype.slice.call(arguments, 2));
+            };
+        }
+    ]);
